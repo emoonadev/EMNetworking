71732d4e727af54c6ef4fcc0a913de0ff372c1b7
Mickael Belhassen <emoonadev@gmail.com>
2024-08-04 14:52:14 +0300

    Minor change

--- Sources/MyMacro/MyMacro.swift
+++ Sources/MyMacro/MyMacro.swift
@@ -8,10 +8,10 @@ import Foundation
 public macro HTTP(_ method: HTTPMethod, path: CNPath...) = #externalMacro(module: "MyMacroMacros", type: "HTTPMethodMacro")
 
 @attached(extension, conformances: APIRoute, names: arbitrary)
-public macro RouteAPI(_ controller: String, baseURL: URL) = #externalMacro(module: "MyMacroMacros", type: "RouteAPI")
+public macro RouteAPI(_ controller: CNPath, baseURL: URL) = #externalMacro(module: "MyMacroMacros", type: "RouteAPI")
 
 @attached(peer, conformances: APIRoute, names: arbitrary)
-public macro RouteAPI(_ name: String) = #externalMacro(module: "MyMacroMacros", type: "RouteAPI")
+public macro RouteAPI(_ name: CNPath) = #externalMacro(module: "MyMacroMacros", type: "RouteAPI")
 
 @freestanding(declaration, names: named(baseURL), arbitrary)
 public macro BaseURL(_ url: String, _ block: () -> Void) = #externalMacro(module: "MyMacroMacros", type: "BaseURL")
--- Sources/MyMacroClient/AppAPI.swift
+++ Sources/MyMacroClient/AppAPI.swift
@@ -12,15 +12,29 @@ enum AppAPI {
 
     #BaseURL("https://www.tinytap.com/") {
         
-        @RouteAPI("account/api/")
+        @RouteAPI(.account)
         enum User {
-            @HTTP(.post, path: "login") case login(LoginReq)
+            @HTTP(.post, path: .login) case login(LoginReq)
         }
 
-        @RouteAPI("community/api/")
+        @RouteAPI(.community)
         enum Community {
-            @HTTP(.get, path: "configurations") case configurations
+            @HTTP(.get, path: .configurations) case configurations
         }
         
     }
+    
+    @RouteAPI("community/api/", baseURL: URL(string: "")!)
+    enum Communityu {
+//        @HTTP(.get, path: .parameter("id")) case configurations(id: Int)
+        @HTTP(.get, path: "configuration", "user", .parameter("id")) case configurations(id: Int)
+    }
+}
+
+extension CNPath {
+    static let community: CNPath = "community/api/"
+    static let account: CNPath = "account/api/"
+    
+    static let login: CNPath = "login"
+    static let configurations: CNPath = "configurations"
 }
