3dae1986c66f09e2b00451aea09485e8fb7d6ca6
Mickael Belhassen <emoonadev@gmail.com>
2024-09-01 15:15:16 +0300

    Minor fix

--- Sources/EMNetworking/Models/ServerResponse.swift
+++ Sources/EMNetworking/Models/ServerResponse.swift
@@ -10,6 +10,11 @@ import Foundation
 public struct ServerResponse<T: Codable>: Codable {
     public var message: String?
     public var data: T?
+    
+    public init(message: String? = nil, data: T? = nil) {
+        self.message = message
+        self.data = data
+    }
 }
 
 public struct Nothing: Codable {
