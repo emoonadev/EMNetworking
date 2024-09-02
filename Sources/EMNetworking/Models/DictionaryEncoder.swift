7de116a6cb605793e0c29ed2d37fa3bf105f3ee5
Mickael Belhassen <emoonadev@gmail.com>
2024-09-02 10:34:49 +0300

    public DictionaryEncoder

--- Sources/EMNetworking/Models/DictionaryEncoder.swift
+++ Sources/EMNetworking/Models/DictionaryEncoder.swift
@@ -7,9 +7,9 @@
 
 import Foundation
 
-struct DictionaryEncoder {
+public struct DictionaryEncoder {
 
-    static func encode<T>(_ value: T) throws -> [String: Any] where T: Encodable {
+    public static func encode<T>(_ value: T) throws -> [String: Any] where T: Encodable {
         let encoder = JSONEncoder()
         encoder.dateEncodingStrategy = .iso8601
 
@@ -17,7 +17,7 @@ struct DictionaryEncoder {
         return try JSONSerialization.jsonObject(with: jsonData) as? [String: Any] ?? [:]
     }
 
-    static func encode(data: Data) throws -> [String: Any]? {
+    public static func encode(data: Data) throws -> [String: Any]? {
         return try JSONSerialization.jsonObject(with: data) as? [String: Any]
     }
 
