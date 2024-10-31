e516c04fa458fb6afaddac6e1462f64ad3f82915
Mickael Belhassen <emoonadev@gmail.com>
2024-10-31 10:11:44 +0200

    Added headers to log handler

--- Sources/EMNetworkingClient/main.swift
+++ Sources/EMNetworkingClient/main.swift
@@ -4,18 +4,13 @@ import EMNetworking
 
 Task {
     do {
-//        let req = LoginReq(
-//            auth: "mickael@tinytap.com",
-//            password: "123456",
-//            kidNickName: "nickname",
-//            languageID: 1,
-//            ageGroupID: 2,
-//            userType: 1
-//        )
-//        
-//        let res: LoginRes? = try await networkManager.perform(route: AppAPI.Community.login(req))
-//        
-//        print("Response \(String(describing: res))")
+        let req = LoginReq(
+            languageID: 1
+        )
+        
+        let res: LoginRes? = try await networkManager.perform(route: AppAPI.Community.login(req))
+        
+        print("Response \(String(describing: res))")
     } catch {}
 }
 
