702e0bb44686439198ee12e3ff3709fb9b3c877e
Mickael Belhassen <emoonadev@gmail.com>
2024-12-30 16:17:51 +0200

    Support server environments

--- Sources/EMNetworkingClient/AppAPI.swift
+++ Sources/EMNetworkingClient/AppAPI.swift
@@ -5,12 +5,70 @@
 //  Created by Mickael Belhassen on 01/08/2024.
 //
 
-import Foundation
 import EMNetworking
+import Foundation
 
 enum AppAPI {
 
-    #BaseURL("https://www.tinytap.com/") {
+//    #BaseURL("https://www.tinytap.com/") {
+//
+//        @RouteAPI("community/api/")
+//        enum Community {
+//            @HTTP(.post, path: "login") case login(LoginReq)
+//        }
+//
+//        @RouteAPI("account/api/")
+//        enum Account {
+//            @HTTP(.get, path: "email_lookup") case emailLookup(dto: EmailLookupReq)
+//            @HTTP(.get, path: "profile", .parameter("id"), "history") case profile(id: Int)
+//        }
+//    }
+
+//
+//    #BaseURL("https://www.tinytap.com/") {
+//
+//        @RouteAPI("community/api/")
+//        enum Community {
+//            @HTTP(.post, path: "login") case login(LoginReq)
+//        }
+//
+//        @RouteAPI("account/api/")
+//        enum Account {
+//            @HTTP(.get, path: "email_lookup") case emailLookup(dto: EmailLookupReq)
+//            @HTTP(.get, path: "profile", .parameter("id"), "history") case profile(id: Int)
+//        }
+//    }
+
+//    #BaseURL(BaseURL("https://www.tinytap.com/", staging: "https://www.staging.tinytap.com/")) {
+//
+//        @RouteAPI("community/api/")
+//        enum Community {
+//            @HTTP(.post, path: "login") case login(LoginReq)
+//        }
+//
+//        @RouteAPI("account/api/")
+//        enum Account {
+//            @HTTP(.get, path: "email_lookup") case emailLookup(dto: EmailLookupReq)
+//            @HTTP(.get, path: "profile", .parameter("id"), "history") case profile(id: Int)
+//        }
+//    }
+
+//    #BaseURL(BaseURL(URL(string: "https://www.tinytap.com/")!)) {
+//
+//        @RouteAPI("community/api/")
+//        enum Community {
+//            @HTTP(.post, path: "login") case login(LoginReq)
+//        }
+//
+//        @RouteAPI("account/api/")
+//        enum Account {
+//            @HTTP(.get, path: "email_lookup") case emailLookup(dto: EmailLookupReq)
+//            @HTTP(.get, path: "profile", .parameter("id"), "history") case profile(id: Int)
+//        }
+//    }
+
+
+    #BaseURL(BaseURL("https://www.tinytap.com/", dev: "https://www.dev.tinytap.com/")) {
 
         @RouteAPI("community/api/")
         enum Community {
@@ -23,5 +81,6 @@ enum AppAPI {
             @HTTP(.get, path: "profile", .parameter("id"), "history") case profile(id: Int)
         }
     }
-    
+
+
 }
--- Sources/EMNetworkingMacros/BaseURL.swift
+++ Sources/EMNetworkingMacros/BaseURL.swift
@@ -1,41 +1,44 @@
 //
 //  BaseURL.swift
-//  
+//
 //
 //  Created by Mickael Belhassen on 01/08/2024.
 //
 
-import SwiftCompilerPlugin
 import Foundation
+import SwiftCompilerPlugin
 import SwiftSyntax
 import SwiftSyntaxBuilder
 import SwiftSyntaxMacros
 
 enum BaseURL: DeclarationMacro {
-    
-    static func expansion(of node: some FreestandingMacroExpansionSyntax, in context: some MacroExpansionContext) throws -> [DeclSyntax] {
-        guard let baseURLParameter = node.arguments.first?.expression,
-              let baseURL = baseURLParameter.as(StringLiteralExprSyntax.self)?.representedLiteralValue,
-              let block = node.trailingClosure else {
-            return []
+
+    static func expansion(of node: some FreestandingMacroExpansionSyntax, in _: some MacroExpansionContext) throws -> [DeclSyntax] {
+        if let baseURLParameter = node.arguments.first?.expression, let block = node.trailingClosure {
+            if let baseURL = baseURLParameter.as(StringLiteralExprSyntax.self)?.representedLiteralValue {
+                block.statements.compactMap { $0.item.asProtocol(DeclGroupSyntax.self) }.map { replacingRouteAPIAttribute(of: $0, parameterURL: baseURL) }
+            } else if let expression = node.arguments.first?.expression.as(FunctionCallExprSyntax.self), expression.calledExpression.as(DeclReferenceExprSyntax.self)?.baseName.identifier?.name == "BaseURL" {
+                block.statements.compactMap { $0.item.asProtocol(DeclGroupSyntax.self) }.map { replacingRouteAPIAttribute(of: $0, parameterURL: "\(node.arguments)") }
+            } else {
+                []
+            }
+        } else {
+            []
         }
-        let decls = block.statements.compactMap { $0.item.asProtocol(DeclGroupSyntax.self) }
-        
-        return decls.map { replacingRouteAPIAttribute(of: $0, baseURL: baseURL) }
     }
-    
-    static func replacingRouteAPIAttribute(of decl: DeclGroupSyntax, baseURL: String) -> DeclSyntax {
+
+    static func replacingRouteAPIAttribute(of decl: DeclGroupSyntax, parameterURL: String) -> DeclSyntax {
         var newDecl = decl
         for i in newDecl.attributes.indices {
             guard case var .attribute(attr) = newDecl.attributes[i],
                   let attrName = attr.attributeName.as(IdentifierTypeSyntax.self)?.name.text,
-                  attrName == "RouteAPI" ,
+                  attrName == "RouteAPI",
                   case let .argumentList(argsList) = attr.arguments,
                   argsList.count == 1
             else {
                 continue
             }
-            let expr: ExprSyntax = "URL(string: \(literal: baseURL))!"
+            let expr: ExprSyntax = parameterURL.starts(with: "BaseURL") ? .init(stringLiteral: parameterURL) : "BaseURL(\(literal: parameterURL))"
             let newArgsList = LabeledExprListSyntax {
                 argsList
                 LabeledExprSyntax(label: "baseURL", expression: expr)
